9090d01245193b5df314490caf96971c3a152c94
202210076 <maxium153@cuk.edu>
2025-06-03 15:01:32 +0900

    주석 업데이트

--- chatbot.py
+++ chatbot.py
@@ -4,19 +4,19 @@ from Levenshtein import distance # python-Levenshtein 패키지에서 Levenshtei
 # 챗봇 객체 정의
 class SimpleChatBot:
     """
-        SimpleChatBot 클래스는 Q(Question), A(Answer) 가 있는 CSV 파일을 Parsing 하여 Input 질문에 대한 가장 유사한 A(Answer) 를 제공
+        SimpleChatBot 클래스는 Q(Question), A(Answer) 가 있는 CSV 파일을 Parsing 하여 Input 질문에 대한 가장 유사한 A(Answer) 를 제공.
 
         Attributes:
             _data (list): csv 파일의 Q, A 값에 대한 list
 
         Method:
-            answer(question: str) -> str: 입력된 질문과 CSV에서 불러온 모든 질문의 Levenshtein 거리를 계산하여 가장 유사한 질문의 답변을 반환 (문자열 return).
+            answer(question: str) -> str: 입력된 질문과 CSV에서 불러온 모든 질문의 Levenshtein 거리를 계산하여 가장 유사한 질문의 답변을 반환. (str return)
     """
     def __init__(self, file="ChatbotData.csv"):
         """
-        질문과 답변이 있는 csv 파일을 Loading 각각 Q(질문), A(답변)을 리스트 형태로 저장
-        해당 맴버 변수는 Private Member 변수로 외부 에서 값을 수정할 수 없도록 처리
-        :param self: SimpleChatBot. 객체
+        질문과 답변이 있는 csv 파일을 Loading 각각 Q(질문), A(답변)을 리스트 형태로 저장.
+        해당 맴버 변수는 Private Member 변수로 외부 에서 값을 수정할 수 없도록 처리.
+        :param self: SimpleChatBot 객체
         :param file: 질문과 답변이 있는 csv 파일 경로 (기본값 "ChatbotData.csv")
         """
         self._data = pd.read_csv(file)[['Q', 'A']].values.tolist()
@@ -24,7 +24,7 @@ class SimpleChatBot:
     def answer(self, question: str) -> str:
         """
         입력된 질문과 CSV에서 불러온 모든 질문의 Levenshtein 거리를 계산하여 가장 유사한 질문의 답변을 반환.
-        :param self: SimpleChatBot. 객체
+        :param self: SimpleChatBot 객체
         :param question: 사용자가 입력한 질문 (str)
         :return: 가장 유사한 답변 (str)
         """
@@ -37,7 +37,7 @@ class SimpleChatBot:
             currentDistance = distance(question, Q)
             # 만약 현재 거리가 지금까지의 최소 거리보다 작다면,
             if currentDistance < minDistance:
-                minDistance = currentDistance  # 최소 거리를 갱신합니다.
+                minDistance = currentDistance  # 최소 거리를 갱신.
                 bestAnswer = A                 # 해당 질문의 답변을 최적 답변으로 저장.
-        # 모든 쌍을 비교한 후, 최적의 답변을을 반환
+        # 모든 쌍을 비교한 후, 최적의 답변을 반환
         return bestAnswer
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     # '종료'라는 입력이 나올 때까지 사용자의 입력에 따라 챗봇의 응답을 출력하는 무한 루프를 실행.
     while True:
-        question = input('You: ') # 입력 확인
+        question = input('You: ') # 입력 확인.
 
         if question.lower() == '종료':
             print('Chatbot: Chatbot을 종료합니다.')
